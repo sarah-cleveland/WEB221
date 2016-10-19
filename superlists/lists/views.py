d827da7270f6df3f73dd7c678b160687927de575
sarahcleveland <sarah.cleveland@mrm-mccann.com>
2016-10-19 00:20:58 -0400

    enforce model validation in list view

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from lists.models import Item, List
-from django.core.exceptions import ValidationError
 
 def home_page(request):
     return render(request, 'home.html')
@@ -8,21 +8,28 @@ def home_page(request):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
     try:
-    	item.full_clean()
-    	item.save()
+        item.full_clean()
+        item.save()
     except ValidationError:
-    	list_.delete()
-    	error = "You can't have an empty list item"
-    	return render(request, 'home.html', {"error": error})
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
 
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    if request.method == 'POST':
-    	Item.objects.create(text=request.POST['item_text'], list=list_)
-    	return redirect('/lists/%d/' % (list_.id,))
-    return render(request, 'list.html', {'list': list_})
+    error = None
 
+    if request.method == 'POST':
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect('/lists/%d/' % (list_.id,))
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {'list': list_, 'error': error})
