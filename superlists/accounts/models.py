5401e8e87bf912476f654df7d17d5fb2a1f32874
sarahcleveland <sarah.cleveland@mrm-mccann.com>
2016-10-30 16:00:16 -0400

    end of chapter 15

---
+++ superlists/accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid
+
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+    REQUIRED_FIELDS = ()
+    USERNAME_FIELD = 'email'
+
+    def is_authenticated(self):
+        return True
+
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
